465a3d4f7e0aeced8ccea27e72b733adcfa93248
Paul Hoger <paul.hoger@student.kit.edu>
2025-07-04 19:18:01 +0200

    Initialize Java project

---
+++ discord-bot/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "de.paul2708.cs2stats"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
