639827111e6dfdc4c8dc708eb55bddb8c1c3c0d5
Paul Hoger <paul.hoger@student.kit.edu>
2025-07-04 19:21:16 +0200

    Add JDA dependency

--- discord-bot/build.gradle.kts
+++ discord-bot/build.gradle.kts
@@ -1,19 +1,27 @@
 plugins {
     id("java")
+
+    application
+    id("com.gradleup.shadow") version "8.3.1"
 }
 
+application.mainClass = "de.paul2708.csstats.Main"
 group = "de.paul2708.cs2stats"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
+
+val jdaVersion = "5.6.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.dv8tion:JDA:$jdaVersion")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.isIncremental = true
+
+    sourceCompatibility = "21"
 }
