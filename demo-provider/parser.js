bc1c9b1669bc31abc3eef83e259ccd10244192c3
Paul Hoger <paul.hoger@student.kit.edu>
2025-07-04 18:49:18 +0200

    Implement demo parser by utilizing demoparser2

---
+++ demo-provider/parser.js
@@ -0,0 +1,50 @@
+const {parseHeader, parseEvent, parseTicks} = require("@laihoe/demoparser2");
+
+function parseDemo(demoPath) {
+    let gameEndTick = Math.max(...parseEvent(demoPath, "round_end").map(x => x.tick))
+
+    let fields = [
+        "kills_total", "deaths_total",
+        "mvps",
+        "rank_if_win", "rank_if_loss", "rank_if_tie",
+        "team_score_first_half", "team_score_second_half", "total_rounds_played"
+    ]
+    let scoreboard = parseTicks(demoPath, fields, [gameEndTick])
+    console.log(scoreboard);
+
+    const playerStats = []
+
+    for (let i = 0; i < scoreboard.length; i++) {
+        const player = scoreboard[i];
+
+        let rank;
+        const totalRounds = player.total_rounds_played;
+        const teamRounds = player.team_score_first_half + player.team_score_second_half;
+        const enemyRounds = totalRounds - teamRounds;
+
+        if (totalRounds > enemyRounds) {
+            rank = player.rank_if_win;
+        } else if (totalRounds < enemyRounds) {
+            rank = player.rank_if_loss;
+        } else {
+            rank = player.rank_if_tie;
+        }
+
+        playerStats.push({
+            "name": player.name,
+            "steamId": player.steamid,
+            "deaths": player.deaths_total,
+            "kills": player.kills_total,
+            "mvps": player.mvps,
+            "updatedRank": rank,
+        })
+    }
+
+    return {
+        map: parseHeader(demoPath)["map_name"],
+        scoreboard: playerStats,
+    };
+}
+
+
+module.exports = {parseDemo};
