118f205f2fb4c2579517132c88629921446faca6
Paul Hoger <paul.hoger@student.kit.edu>
2025-07-04 18:50:11 +0200

    Implement demo endpoint providing match details

--- demo-provider/index.js
+++ demo-provider/index.js
@@ -1,11 +1,35 @@
 require('dotenv').config()
 
 const express = require('express')
+const {connectToSteam, requestGame} = require("./steam");
+const {downloadDemo} = require("./downloader");
+const {parseDemo} = require("./parser");
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send(`Hello ${process.env.STEAM_USERNAME}`)
+connectToSteam(process.env.STEAM_USERNAME, process.env.STEAM_PASSWORD)
+
+app.get('/demo/:shareCode', (req, res) => {
+    requestGame(req.params.shareCode, (matchResponse) => {
+        const matchId = matchResponse.matchId;
+        const demoUrl = matchResponse.demoUrl;
+
+        console.log("Fetched: " + matchId, demoUrl)
+
+        downloadDemo(demoUrl, (path) => {
+            if (path.startsWith("error")) {
+                res.status(500).send(path);
+            } else {
+
+                const response = {
+                    matchId: matchId,
+                    demoUrl: demoUrl,
+                    match: parseDemo(path),
+                }
+                res.status(200).send(response)
+            }
+        })
+    })
 })
 
 app.listen(port, () => {
